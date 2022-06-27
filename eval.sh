db088265ed147fece32d55c9c01fa053e05ec7b2
K-H-Ismail <khalfaoui.ismail@hotmail.com>
2022-06-27 16:40:02 +0200

    minor

--- eval.sh
+++ eval.sh
@@ -3,13 +3,13 @@ echo 'using' $NUM_PROC 'gpu(s)'
 dataset='ImageNet'
 root='/home/ismail'
 model=convnext_tiny
-output=output_convnext_tiny_ImageNet_11_06_2022_22:37:36
+output=paper/output_convnext_tiny_ImageNet_dcls
 
 CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node=$NUM_PROC --master_port 47781 $root/ConvNeXt/main.py \
 --model $model --eval true --drop_path 0.1 --input_size 224 --nb_classes 1000 \
 --data_path $root/datasets/$dataset/2012 \
 --resume $root/ConvNeXt/outputs/$output/checkpoint-best-ema.pth \
---use_dcls 'yes' --dcls_kernel_size 17 --dcls_kernel_count 34 --dcls_sync 'yes' --use_loss_rep 'yes' \
+--use_dcls 'yes' --dcls_kernel_size 17 --dcls_kernel_count 34 --dcls_sync 'yes' --use_loss_rep 'yes' --show_erf 'yes' \
 --model_ema true --model_ema_eval true \
 
 
