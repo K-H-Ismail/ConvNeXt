8c4e8754af350dd58407a1735c93f099b6c54f75
K-H-Ismail <khalfaoui.ismail@hotmail.com>
2022-06-27 13:22:24 +0200

    submitit run adapt to cluster

--- run_with_submitit.py
+++ run_with_submitit.py
@@ -22,16 +22,18 @@ def parse_args():
     parser.add_argument("--timeout", default=72, type=int, help="Duration of the job, in hours")
     parser.add_argument("--job_name", default="convnext", type=str, help="Job name")
     parser.add_argument("--job_dir", default="", type=str, help="Job directory; leave empty for default")
-    parser.add_argument("--partition", default="learnlab", type=str, help="Partition where to submit")
-    parser.add_argument("--use_volta32", action='store_true', default=True, help="Big models? Use this")
+    parser.add_argument("--partition", default="gpu_p2", type=str, help="Partition where to submit")
+    parser.add_argument("--use_volta32", action='store_true', default=False, help="Big models? Use this")
+    parser.add_argument("--account", default="owj@v100", type=str, help="Account name")
+    parser.add_argument("--constraint", default="v100", type=str, help="constraint v100 or a100")
     parser.add_argument('--comment', default="", type=str,
                         help='Comment to pass to scheduler, e.g. priority message')
     return parser.parse_args()
 
 def get_shared_folder() -> Path:
-    user = os.getenv("USER")
-    if Path("/checkpoint/").is_dir():
-        p = Path(f"/checkpoint/{user}/convnext")
+    work = os.getenv("WORK")
+    if Path(f"{work}/ConvNeXt/checkpoint").is_dir():
+        p = Path(f"{work}/ConvNeXt/checkpoint")
         p.mkdir(exist_ok=True)
         return p
     raise RuntimeError("No shared folder available")
@@ -78,7 +80,7 @@ class Trainer(object):
 
 def main():
     args = parse_args()
-    
+
     if args.job_dir == "":
         args.job_dir = get_shared_folder() / "%j"
 
@@ -91,15 +93,16 @@ def main():
     partition = args.partition
     kwargs = {}
     if args.use_volta32:
-        kwargs['slurm_constraint'] = 'volta32gb'
+        kwargs['slurm_constraint'] = 'v100-32g'
     if args.comment:
         kwargs['slurm_comment'] = args.comment
 
     executor.update_parameters(
-        mem_gb=40 * num_gpus_per_node,
+        #mem_gb=40 * num_gpus_per_node,
         gpus_per_node=num_gpus_per_node,
         tasks_per_node=num_gpus_per_node,  # one task per GPU
-        cpus_per_task=10,
+        cpus_per_task=args.num_workers,
+        slurm_account=args.account,
         nodes=nodes,
         timeout_min=timeout_min,  # max is 60 * 72
         # Below are cluster dependent parameters
@@ -120,3 +123,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
