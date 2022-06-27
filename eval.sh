fa2290fa5008c3b0be0f456fddacb0c99703037d
K-H-Ismail <khalfaoui.ismail@hotmail.com>
2022-06-27 13:23:20 +0200

    minor

--- eval.sh
+++ eval.sh
@@ -1,6 +1,18 @@
-python3 -m torch.distributed.launch --nproc_per_node=2 main.py \
---model convnext_large --eval true \
---resume https://dl.fbaipublicfiles.com/convnext/convnext_large_1k_224_ema.pth \
---input_size 224 --drop_path 0.2 \
---data_path /home/ismail/ImageNet/2012 \
---nb_classes 1000
+NUM_PROC=2
+echo 'using' $NUM_PROC 'gpu(s)'
+dataset='ImageNet'
+root='/home/ismail'
+model=convnext_tiny
+output=output_convnext_tiny_ImageNet_11_06_2022_22:37:36
+
+CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node=$NUM_PROC --master_port 47781 $root/ConvNeXt/main.py \
+--model $model --eval true --drop_path 0.1 --input_size 224 --nb_classes 1000 \
+--data_path $root/datasets/$dataset/2012 \
+--resume $root/ConvNeXt/outputs/$output/checkpoint-best-ema.pth \
+--use_dcls 'yes' --dcls_kernel_size 17 --dcls_kernel_count 34 --dcls_sync 'yes' --use_loss_rep 'yes' \
+--model_ema true --model_ema_eval true \
+
+
+
+
+
--- run.sh
+++ run.sh
@@ -1,14 +1,22 @@
+NUM_PROC=2
+echo 'using' $NUM_PROC 'gpu(s)'
+BATCHSIZE=256
+UP_FREQ=$((4096 / (BATCHSIZE * NUM_PROC)))
+
+echo 'using' $UP_FREQ 'updates'
+
 time=$(date '+%d_%m_%Y_%H:%M:%S')
-dataset='ImageNet48'
+dataset='ImageNet192'
 root='/home/ismail'
 model=convnext_tiny
 
-CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 47781 ./main.py \
---model $model --drop_path 0.1 --batch_size 256 --lr 4e-3 \
---update_freq 8 --input_size 224 --nb_classes 1000 \
+CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node=$NUM_PROC --master_port 47781 $root/ConvNeXt/main.py \
+--model $model --drop_path 0.1 --batch_size $BATCHSIZE --lr 4e-3 \
+--update_freq $UP_FREQ --input_size 224 --nb_classes 1000 \
 --data_path $root/datasets/$dataset/2012 \
 --output $root/ConvNeXt/outputs/output_${model}_${dataset}_${time} \
+--use_dcls 'yes' --dcls_kernel_size 17 --dcls_kernel_count 34 --dcls_sync 'yes' --use_loss_rep 'yes' \
 --seed 0 --enable_wandb 'yes' \
---use_dcls 'yes' --dcls_kernel_size 7 --dcls_kernel_count 7 --dcls_sync 'yes' --use_loss_rep 'yes'
-#--auto_resume true --resume "/home/ismail/ConvNeXt/outputs/output_convnext_tiny_ImageNet48_28_02_2022_15:31:04/checkpoint-42.pth" \
+#--auto_resume 'yes' --output_dir '/gpfswork/rech/owj/uvr14im/ConvNeXt/outputs/output_convnext_tiny_ImageNet_03_06_2022_15:04:03' \
 #--model_ema true --model_ema_eval true \
+
